c8dcea92d17697223a4bbf2e30481c57f5d57698
Mark Sobkow <mark.sobkow@gmail.com>
2026-03-26 17:14:06 -0600

    2026-03-26 17:10 CST Ready to redo the security initialization code, but this time fully fleshed with the new model in place

--- src/main/java/org/msscf/msscf/v2_13/cfint/CFIntSaxLoader/CFIntSaxLoader.java
+++ src/main/java/org/msscf/msscf/v2_13/cfint/CFIntSaxLoader/CFIntSaxLoader.java
@@ -56,14 +56,15 @@ public class CFIntSaxLoader
 {
 
 	// The namespace URI of the supported schema
-	public final static String	SCHEMA_XMLNS = "uri://org.msscf/msscf/2.0.13/cfintloader";
+	public final static String	SCHEMA_XMLNS = "http://mcf.markhome.server/msscf/2.0.13/cfint-structured.xsd";
 
 	// The source for loading the supported schema
-	public final static String	SCHEMA_URI = "/opt/msscf/2.0.13/xsd/cfint-structured.xsd";
-	public final static String	SCHEMA_ROOT_URI = "/xsd/cfint-structured.xsd";
-	public final static String CFSEC_XMLNS = "uri://org.msscf/msscf/2.0.13/cfsecloader";
-	public final static String CFSEC_URI = "/opt/msscf/2.0.13/xsd/cfsec-structured.xsd";
-	public final static String CFSEC_ROOT_URI = "/xsd/cfsec-structured.xsd";
+	public final static String	SCHEMA_URI = "http://mcf.markhome.server/msscf/2.0.13/cfint-structured.xsd";
+	public final static String	SCHEMA_ROOT_URI = "http://mcf.markhome.server/msscf/2.0.13/cfint-structured.xsd";
+
+	public final static String CFSEC_XMLNS = "http://mcf.markhome.server/msscf/2.0.13/cfsec-structured.xsd";
+	public final static String CFSEC_URI = "http://mcf.markhome.server/msscf/2.0.13/cfsec-structured.xsd";
+	public final static String CFSEC_ROOT_URI = "http://mcf.markhome.server/msscf/2.0.13/cfsec-structured.xsd";
 
 
 	// The schema instance to load in to
